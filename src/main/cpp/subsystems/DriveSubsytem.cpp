c27c2a5bd91956e7bf07587e8c23ee0e0497949b
Kellen Watt <kellen@wattsoft.dev>
2022-02-19 20:39:18 -0600

    Implement drive subsystem

--- src/main/cpp/subsystems/DriveSubsytem.cpp
+++ src/main/cpp/subsystems/DriveSubsytem.cpp
@@ -1,1 +1,39 @@
 #include "subsystems/DriveSubsytem.h"
+
+DriveSubsystem::DriveSubsystem() : DriveSubsystem(constants::drive::DEFAULT_DEADBAND) {}
+
+DriveSubsystem::DriveSubsystem(double deadband) {
+    mecanumDrive.SetDeadband(deadband);
+    resetDistance();
+    resetGyro();
+}
+
+void DriveSubsystem::drive(double xSpeed, double ySpeed, double rotation) {
+    mecanumDrive.DriveCartesian(ySpeed, xSpeed, rotation);
+}
+
+double DriveSubsystem::distance() {
+    double total = 0;
+    eachMotor([&total](rev::CANSparkMax& m){
+       total += m.GetEncoder().GetPosition();
+    });
+    return total / constants::drive::MOTOR_COUNT;
+}
+
+void DriveSubsystem::resetDistance() {
+    eachMotor([](rev::CANSparkMax& m) {
+        m.GetEncoder().SetPosition(0);
+    });
+}
+
+void DriveSubsystem::resetGyro() {
+    gyro.Reset();
+}
+
+double DriveSubsystem::orientation() {
+    return gyro.GetYaw();
+}
+
+bool DriveSubsystem::seesLine() {
+    return lineSensor.Get();
+}
--- src/main/include/subsystems/DriveSubsytem.h
+++ src/main/include/subsystems/DriveSubsytem.h
@@ -1,1 +1,47 @@
 #pragma once
+
+#include "Constants.h"
+
+#include <functional>
+#include <array>
+
+#include <frc2/command/SubsystemBase.h>
+#include <rev/CANSparkMax.h>
+#include <frc/drive/MecanumDrive.h>
+#include <frc/DigitalInput.h>
+#include <AHRS.h>
+#include <frc/SPI.h>
+
+class DriveSubsystem : public frc2::SubsystemBase {
+public:
+    DriveSubsystem();
+    DriveSubsystem(double deadband);
+    ~DriveSubsystem() noexcept {}
+
+    void drive(double xSpeed, double ySpeed, double rotation);
+
+    double distance();
+    void resetDistance();
+
+    void resetGyro();
+    double orientation();
+
+    bool seesLine();
+private:
+    rev::CANSparkMax frontLeft{constants::drive::FRONT_LEFT_MOTOR, rev::CANSparkMax::MotorType::kBrushless};
+    rev::CANSparkMax rearLeft{constants::drive::REAR_LEFT_MOTOR, rev::CANSparkMax::MotorType::kBrushless};
+    rev::CANSparkMax frontRight{constants::drive::FRONT_RIGHT_MOTOR, rev::CANSparkMax::MotorType::kBrushless};
+    rev::CANSparkMax rearRight{constants::drive::REAR_RIGHT_MOTOR, rev::CANSparkMax::MotorType::kBrushless};
+    frc::MecanumDrive mecanumDrive{frontLeft, rearLeft, frontRight, rearRight}; 
+
+    AHRS gyro{frc::SPI::Port::kMXP};    
+
+    frc::DigitalInput lineSensor{constants::drive::LINE_SENSOR};
+
+    void eachMotor(std::function<void(rev::CANSparkMax&)> f) {
+        f(frontLeft);
+        f(rearLeft);
+        f(frontRight);
+        f(rearRight);
+    }
+};
