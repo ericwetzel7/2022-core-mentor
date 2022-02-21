f3b0dd468e6c575ecc905733a840af414f69f8a4
Kellen Watt <kellen@wattsoft.dev>
2022-02-21 17:15:53 -0600

    Implement default flywheel command

--- src/main/cpp/commands/FlywheelCommand.cpp
+++ src/main/cpp/commands/FlywheelCommand.cpp
@@ -1,1 +1,9 @@
 #include "commands/FlywheelCommand.h"
+
+FlywheelCommand::FlywheelCommand(ShooterSubsystem* shoot) : shooter(shoot) {
+    AddRequirements(shoot);
+}
+
+void FlywheelCommand::Execute() {
+    shooter->enableFlywheel();
+}
--- src/main/include/commands/FlywheelCommand.h
+++ src/main/include/commands/FlywheelCommand.h
@@ -1,1 +1,15 @@
 #pragma once
+
+#include <frc2/command/CommandBase.h>
+#include <frc2/command/CommandHelper.h>
+
+#include "subsystems/ShooterSubsystem.h"
+
+class FlywheelCommand : public frc2::CommandHelper<frc2::CommandBase, FlywheelCommand> {
+public:
+    FlywheelCommand(ShooterSubsystem* shoot);
+
+    void Execute() override;
+private:
+    ShooterSubsystem* shooter;
+};
