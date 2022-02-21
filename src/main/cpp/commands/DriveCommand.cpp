9dae74ea8970e7fc07eee55c504c84a40620d718
Kellen Watt <kellen@wattsoft.dev>
2022-02-21 10:48:38 -0600

    Implement Basic drive command

--- src/main/cpp/commands/DriveCommand.cpp
+++ src/main/cpp/commands/DriveCommand.cpp
@@ -1,1 +1,10 @@
 #include "commands/DriveCommand.h"
+
+DriveCommand::DriveCommand(DriveSubsystem* drive, frc::Joystick* stick) :
+        driveSubsystem(drive), control(stick) {
+    AddRequirements(driveSubsystem);
+}
+
+void DriveCommand::Execute() {
+    driveSubsystem->drive(control->GetX(), control->GetY(), control->GetZ());
+}
--- src/main/include/commands/DriveCommand.h
+++ src/main/include/commands/DriveCommand.h
@@ -1,1 +1,17 @@
 #pragma once
+
+#include <frc2/command/CommandBase.h>
+#include <frc2/command/CommandHelper.h>
+
+#include "subsystems/DriveSubsytem.h"
+#include "frc/Joystick.h"
+
+class DriveCommand : public frc2::CommandHelper<frc2::CommandBase, DriveCommand> {
+public:
+    DriveCommand(DriveSubsystem* drive, frc::Joystick* stick);
+
+    void Execute() override;
+private:
+    DriveSubsystem* driveSubsystem;
+    frc::Joystick* control;
+};
