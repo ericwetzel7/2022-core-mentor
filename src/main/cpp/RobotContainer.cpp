466354e4c9f85e36a412c0fbc2aa8da1d221ff71
Kellen Watt <kellen@wattsoft.dev>
2022-03-01 01:35:42 -0600

    Define several command triggers
    - extending/retracting intake on 2-2
    - running intake on 2-1
    - transfer outer ball inward when possible
    - shifting ball from transport to shooter

--- src/main/cpp/RobotContainer.cpp
+++ src/main/cpp/RobotContainer.cpp
@@ -6,9 +6,18 @@
 
 #include <frc/DriverStation.h>
 
+#include <frc2/command/button/JoystickButton.h>
+#include <frc2/command/StartEndCommand.h>
+#include <frc2/command/SequentialCommandGroup.h>
+#include <frc2/command/InstantCommand.h>
+#include <frc2/command/WaitCommand.h>
+#include <frc2/command/button/Trigger.h>
+
+#include <units/time.h>
+
 #include "commands/DriveCommand.h"
 #include "commands/FlywheelCommand.h"
-
+#include "commands/InwardShiftCommand.h"
 
 RobotContainer::RobotContainer() : transportSubsystem(frc::DriverStation::GetAlliance()) {
   // Initialize all of your commands and subsystems here
@@ -21,5 +30,27 @@ RobotContainer::RobotContainer() : transportSubsystem(frc::DriverStation::GetAll
 
 void RobotContainer::ConfigureButtonBindings() {
   // Configure your button bindings here
+  frc2::JoystickButton(&control2, 2).ToggleWhenPressed(frc2::StartEndCommand(
+    [this] {this->intakeSubsystem.extendArm();},
+    [this] {this->intakeSubsystem.retractArm();},
+    {&intakeSubsystem}
+  ));
+  
+  frc2::JoystickButton(&control2, 1).WhenHeld(frc2::StartEndCommand(
+    [this] {this->intakeSubsystem.startRoller();},
+    [this] {this->intakeSubsystem.stopRoller();},
+    {&intakeSubsystem}
+  ));
+
+  // frc2::Trigger()
+  frc2::Trigger([this]{
+    return transportSubsystem.hasOuterBall() && !transportSubsystem.hasInnerBall();
+  }).WhenActive(InwardShiftCommand(&transportSubsystem));
+
+  auto shootCommand = frc2::SequentialCommandGroup(
+    frc2::InstantCommand([this]{transportSubsystem.enableInnerBelt();}, {&transportSubsystem}),
+    frc2::WaitCommand(0.5_s)
+  );
+  frc2::JoystickButton(&control1, 1).WhenPressed(shootCommand);
 }
 
